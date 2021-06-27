37f1c8bcbcbe9db0fe00b9c41b6b4141bf1ed577
DaemonWhite <44682988+DaemonWhite@users.noreply.github.com>
2021-06-27 23:44:40 +0200

    Nouvelle fonctionaliter
    Il manque le probléme de taille de l'image et elle est préte à etres utiliser

--- src/main.rs
+++ src/main.rs
@@ -19,25 +19,68 @@ fn main() {
         let message_output : gtk::Label = builder.object("message_out").unwrap();
         let image_output : gtk::Image = builder.object("img_out").unwrap();
         let image_output_clone = image_output.clone();
-        let ret_switch : gtk::Switch = builder.object("return_btn").unwrap();
+        let ret_switch : gtk::Switch = builder.object("ret_btn").unwrap();
         let brad_switch : gtk::Switch = builder.object("brad_btn").unwrap();
+        let math_switch : gtk::Switch = builder.object("math_btn").unwrap();
+        let fire_switch : gtk::Switch = builder.object("fire_btn").unwrap();
 
         button.connect_clicked(move |_| {
             let is_ret = ret_switch.is_active();
             let is_brad = brad_switch.is_active();
+            let is_math = math_switch.is_active();
+            let is_fire = fire_switch.is_active();;
             let message_gstring = message_input.text();
             let message = if message_gstring.as_str().is_empty() { &"Create by DaemonWhite" } else { message_gstring.as_str() };
-            if  is_ret {
+
+            if  is_brad && is_math && is_ret && is_fire {
                 message_output.set_text(&format!("{} ", message,));
-                image_output_clone.set_from_file("./img/deat.jpeg");
+                image_output_clone.set_from_file("./img/1.jpeg");
+            } else if is_brad && is_math && is_ret {
+                message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/2.jpg");
+            } else if is_brad && is_math && is_fire {
+                message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/4.png");
+            } else if is_brad && is_ret && is_fire {
+                message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/7.jpg");
+            } else if is_math && is_ret && is_fire {
+                message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/10.jpeg");
+            } else if is_brad && is_math {
+                message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/3.png");
+            } else if is_brad && is_fire {
+                message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/5.png");
+            } else if is_math && is_fire {
+                message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/6.jpeg");
+            } else if is_ret && is_fire {
+                message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/8.jpeg");
+            } else if is_brad && is_ret {
+                message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/9.jpeg");
+            } else if is_math && is_ret {
+                message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/11.png");
             } else if is_brad {
                 message_output.set_text(&format!("{} ", message,));
-                image_output_clone.set_from_file("./img/pedo.png");
+                image_output_clone.set_from_file("./img/brad.png");
+            } else if is_math {
+                message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/math.jpeg");
+            } else if is_ret {
+                message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/deat.jpeg");
+            } else if is_fire {
+		        message_output.set_text(&format!("{} ", message,));
+                image_output_clone.set_from_file("./img/fire.jpeg");
             } else {
                 message_output.set_text(&format!("{} ", message,));
                 image_output_clone.set_from_file("./img/jojo.jpeg");
             }
-
         image_output_clone.show()
         });
 
